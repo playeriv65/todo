5751571533db9acf4c73c1ce59a6b1f029b78558
playeriv65 <playeriv65@gmail.com>
2026-02-09 16:58:28 -0600

    WIP: seperate addmodal

--- my-todo-react/src/App.jsx
+++ my-todo-react/src/App.jsx
@@ -1,11 +1,14 @@
-import "./styles/style.css";
-import "./styles/todo-board.css";
-import * as API from "./api/api.js"
-import { TodoList } from "./components/Todolist.jsx";
-
 import { useEffect, useState } from "react";
 
-import { XIcon, ListPlusIcon } from "@phosphor-icons/react"
+import "./styles/style.css";
+import "./styles/todo-board.css";
+
+import * as API from "./api/api.js"
+
+import { TodoList } from "./components/Todolist.jsx";
+import { AddModal } from "./components/AddModal.jsx";
+
+
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -19,19 +22,9 @@ function App() {
     loadTodoList();
   }, []);
 
-  const [todoName, setTodoName] = useState("");
-  const [ddl, setDdl] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleAddTodo = async () => {
-    const createdTodo = await API.createTodo({
-      todoName,
-      ddl,
-    });
-
-    setTodoList((prev) => [...prev, createdTodo]);
-    setTodoName("");
-    setDdl("");
+  async function onAddTodo(todoToAdd) {
+    const newTodo = await API.createTodo(todoToAdd);
+    setTodoList((prev) => [...prev, newTodo]);
   }
 
   return (
@@ -39,43 +32,7 @@ function App() {
       <div className="todo-app-div">
         <TodoList todoList={todoList} />
 
-        <div id="add-modal" className="add-modal">
-          <div id="add-bar-overlay" className={`add-bar-overlay ${isModalOpen ? "active" : ""}`}>
-            <div id="add-bar" className="add-bar">
-              <input
-                className="name-input styled-input"
-                placeholder="todo name"
-                id="name-input"
-                value={todoName}
-                onChange={(e) => setTodoName(e.target.value)}
-              />
-
-              <input
-                className="ddl-input styled-input"
-                type="datetime-local"
-                placeholder="ddl"
-                id="ddl-input"
-                value={ddl}
-                onChange={(e) => setDdl(e.target.value)}
-              />
-
-              <button
-                type="button"
-                className="add-button"
-                id="add-button"
-                onClick={() => { handleAddTodo(); }}
-              >
-                Add
-              </button>
-            </div>
-          </div>
-
-          <button id="open-modal-button" className="open-modal-button" onClick={
-            () => setIsModalOpen(v => !v)
-          }>
-            {isModalOpen ? <XIcon size={24} /> : <ListPlusIcon size={24} />}
-          </button>
-        </div>
+        <AddModal onAddTodo={onAddTodo} />
       </div>
     </>
   );
---
+++ my-todo-react/src/components/AddModal.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+import { ListPlusIcon, XIcon, } from "@phosphor-icons/react"
+
+export function AddModal({ onAddTodo }) {
+  const [todoName, setTodoName] = useState("");
+  const [ddl, setDdl] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddTodo = async () => {
+    const todoToAdd = {
+      todoName,
+      ddl,
+    };
+
+    await onAddTodo(todoToAdd);
+    setTodoName("");
+    setDdl("");
+  }
+
+  return (
+    <div id="add-modal" className="add-modal">
+      <div id="add-bar-overlay" className={`add-bar-overlay ${isModalOpen ? "active" : ""}`}>
+        <div id="add-bar" className="add-bar">
+          <input
+            className="name-input styled-input"
+            placeholder="todo name"
+            id="name-input"
+            value={todoName}
+            onChange={(e) => setTodoName(e.target.value)}
+          />
+
+          <input
+            className="ddl-input styled-input"
+            type="datetime-local"
+            placeholder="ddl"
+            id="ddl-input"
+            value={ddl}
+            onChange={(e) => setDdl(e.target.value)}
+          />
+
+          <button
+            type="button"
+            className="add-button"
+            id="add-button"
+            onClick={() => { handleAddTodo(); }}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+
+      <button id="open-modal-button" className="open-modal-button" onClick={
+        () => setIsModalOpen(v => !v)
+      }>
+        {isModalOpen ? <XIcon size={24} /> : <ListPlusIcon size={24} />}
+      </button>
+    </div>
+  )
+}
